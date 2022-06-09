14f4403c4ba8c65a38b67d8eb187713afe82fbd7
Kremogen <nik.kosachev@mail.ru>
2022-06-09 23:20:41 +0300

    Added BD listener

--- app/src/main/java/com/kremogen/pharmacyhelper/PharmacyWindow.java
+++ app/src/main/java/com/kremogen/pharmacyhelper/PharmacyWindow.java
@@ -1,17 +1,62 @@
 package com.kremogen.pharmacyhelper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class PharmacyWindow extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef;
+    private List<String> meds;
+
+    ListView ListMeds;
+    FirebaseUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pharmacy_window);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        ListMeds = (ListView) findViewById(R.id.listView);
+        myRef = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+
+        myRef.child(mUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>(){};
+                meds = dataSnapshot.child("medicines").getValue(t);
+
+                updateUI();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+    private void updateUI() {
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(getBaseContext(), android.R.layout.simple_list_item_1);
+        ListMeds.setAdapter(adapter);
     }
 }
